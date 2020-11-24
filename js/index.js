246165ebe352027cb42ae6c462d2fbd7be49f706
Stanislav <spirit9969@gmail.com>
2020-11-25 00:39:58 +0200

    Edit slider

--- js/index.js
+++ js/index.js
@@ -88,7 +88,8 @@ const listProduct=document.querySelector('.products-list'),
     function cardsView(selector){
         for(const productCard of pructsPrice[`${selector.className}`]){
         selector.innerHTML+=`
-        <article class="product-card">
+        
+        <article class="product-card ${selector.className+'_card'}">
             <img src=${productCard.src} alt="Cake">
             <h3>${productCard.name}</h3>
             <h3>${productCard.price}</h3>
@@ -102,6 +103,55 @@ const listProduct=document.querySelector('.products-list'),
 cardsView(cakes);
 cardsView(cookies);
 cardsView(cupcakes);
+
+//s
+let slideIndex = 1;
+    const slides = document.querySelectorAll('.cakes_card'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
+
+    
 
 });
 
